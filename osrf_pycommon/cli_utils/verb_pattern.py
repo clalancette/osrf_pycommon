342f3fd1e17e7176d0ee35a90fd6b118ed19c84b
Chris Lalancette <clalancette@openrobotics.org>
2020-04-23 14:20:18 +0000

    Remove pkg_resources.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- osrf_pycommon/cli_utils/verb_pattern.py
+++ osrf_pycommon/cli_utils/verb_pattern.py
@@ -16,7 +16,7 @@
 
 import inspect
 
-import pkg_resources
+import importlib_metadata
 
 
 def call_prepare_arguments(func, parser, sysargs=None):
@@ -130,7 +130,7 @@ def list_verbs(group):
     :rtype: list of str
     """
     verbs = []
-    for entry_point in pkg_resources.iter_entry_points(group=group):
+    for entry_point in importlib_metadata.entry_points().get(group, []):
         verbs.append(entry_point.name)
     return verbs
 
@@ -143,7 +143,7 @@ def load_verb_description(verb_name, group):
     :returns: verb description
     :rtype: dict
     """
-    for entry_point in pkg_resources.iter_entry_points(group=group):
+    for entry_point in importlib_metadata.entry_points().get(group, []):
         if entry_point.name == verb_name:
             return entry_point.load()
 
